9dee17c595231f8b4bf063d7034336d2b439fc9a
SupriseSelinda <selindasurprise22@gmail.com>
2024-04-29 11:32:48 +0200

    First commit

---
+++ src/main/java/com/mycompany/practical_advanced_apd/Practical_Advanced_Apd.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.practical_advanced_apd;
+
+/**
+ *
+ * @author Suprise
+ */
+public class Practical_Advanced_Apd {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
